b9fbe5b12a4189bebaf6c930959595780878ea42
twk-mn <fred@tawakemono.com>
2022-11-16 22:02:14 +0900

    Added Geocoding to the project, but not working in the front-end yet.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_015356) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_123246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_015356) do
     t.datetime "updated_at", null: false
     t.string "featured_img"
     t.string "images"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_fields_on_user_id"
   end
 
