8ed419114fbc559554a36090200b26751d88b9a7
Hafid Qarchi <haqarchi@gmail.com>
2022-11-16 09:01:24 +0900

    change the status to integer

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,11 +8,10 @@ class Booking < ApplicationRecord
   validates :date_to, presence: true
   validate :date_to_cannot_be_in_the_past, on: :create
 
-  validates :date_from, presence: true
-  validates :price_per_day, presence: true
+  validate :not_overlapping
   validates :user_id, presence: true
 
-  enum :status, ["pending", "confirmed", "rejected"], default: :pending
+  enum :status, %i[pending confirmed rejected], default: :pending
 
   def date_from_cannot_be_in_the_past
     errors.add(:date_from, "Can't be in the past") if date_from.present? && date_from < Date.today
@@ -28,5 +27,10 @@ class Booking < ApplicationRecord
 
   def days
     (date_to - date_from).to_i
+  end
+
+  def not_overlapping
+    existing_bookings = Booking.where("date_from <= ? AND date_to >= ?", date_to, date_from)
+    errors.add(:base, "Already booked for this days") if existing_bookings.any?
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,95 +3,95 @@
 #   - Fields
 # Will make it look better later...
 
-puts "--- [ Users ] ---"
+# puts "--- [ Users ] ---"
 
-puts " - Removing all users!"
-User.destroy_all
+# puts " - Removing all users!"
+# User.destroy_all
 
-25.times do
-  is_farmer = [true, false].sample
-  User.create!(email: Faker::Internet.email, password: "AwesomePassword!123", name: Faker::Name.name, landowner: is_farmer)
-end
+# 25.times do
+#   is_farmer = [true, false].sample
+#   User.create!(email: Faker::Internet.email, password: "AwesomePassword!123", name: Faker::Name.name, landowner: is_farmer)
+# end
 
-# Test Accounts
-## Renters
-User.create!(email: "blue@renter.com", password: "AwesomePassword!123", name: "Renter Blue", landowner: false)
+# # Test Accounts
+# ## Renters
+# User.create!(email: "blue@renter.com", password: "AwesomePassword!123", name: "Renter Blue", landowner: false)
 
-User.create!(email: "red@renter.com", password: "AwesomePassword!123", name: "Renter Red", landowner: false)
+# User.create!(email: "red@renter.com", password: "AwesomePassword!123", name: "Renter Red", landowner: false)
 
-User.create!(email: "green@renter.com", password: "AwesomePassword!123", name: "Renter Green", landowner: false)
+# User.create!(email: "green@renter.com", password: "AwesomePassword!123", name: "Renter Green", landowner: false)
 
-## Farmers
-User.create!(email: "apple@farmer.com", password: "AwesomePassword!123", name: "Farmer Apple", landowner: true)
+# ## Farmers
+# User.create!(email: "apple@farmer.com", password: "AwesomePassword!123", name: "Farmer Apple", landowner: true)
 
-User.create!(email: "orange@farmer.com", password: "AwesomePassword!123", name: "Farmer Orange", landowner: true)
+# User.create!(email: "orange@farmer.com", password: "AwesomePassword!123", name: "Farmer Orange", landowner: true)
 
-User.create!(email: "pear@farmer.com", password: "AwesomePassword!123", name: "Farmer Pear", landowner: true)
+# User.create!(email: "pear@farmer.com", password: "AwesomePassword!123", name: "Farmer Pear", landowner: true)
 
-puts " - Added #{User.count} users"
+# puts " - Added #{User.count} users"
 
-puts "--- [ Users Done ] ---"
-puts "#####"
-puts "--- [ Fields ] ---"
-puts " - Removing all fields!"
-Field.destroy_all
+# puts "--- [ Users Done ] ---"
+# puts "#####"
+# puts "--- [ Fields ] ---"
+# puts " - Removing all fields!"
+# Field.destroy_all
 
-def field_count
-  puts "... have added #{Field.count} fields. Plz wait! "
-end
-2.times do
-  field_owner = User.where("landowner = 'true'").sample
-  unspash_img = "https://source.unsplash.com/640x360?field"
-  img_1 = URI.parse(unspash_img).open
-  img_2 = URI.parse(unspash_img).open
-  img_3 = URI.parse(unspash_img).open
-  img_4 = URI.parse(unspash_img).open
-  img_5 = URI.parse(unspash_img).open
-  field_images = [img_1, img_2, img_3, img_4, img_5]
-  field = Field.new(name: Faker::Movies::StarWars.planet,
-                    size: rand(50..1500),
-                    description: Faker::Lorem.paragraph_by_chars,
-                    location: Faker::Address.full_address,
-                    price: rand(1500..5000),
-                    user_id: field_owner.id)
-  field_images.each do |image|
-    field.images.attach(io: image, filename: "#{rand(0..9999)}-field.png", content_type: "image/png")
-  end
-  field.featured_img = field.images.first.key
-  field.save
-  field_count
-end
+# def field_count
+#   puts "... have added #{Field.count} fields. Plz wait! "
+# end
+# 2.times do
+#   field_owner = User.where("landowner = 'true'").sample
+#   unspash_img = "https://source.unsplash.com/640x360?field"
+#   img_1 = URI.parse(unspash_img).open
+#   img_2 = URI.parse(unspash_img).open
+#   img_3 = URI.parse(unspash_img).open
+#   img_4 = URI.parse(unspash_img).open
+#   img_5 = URI.parse(unspash_img).open
+#   field_images = [img_1, img_2, img_3, img_4, img_5]
+#   field = Field.new(name: Faker::Movies::StarWars.planet,
+#                     size: rand(50..1500),
+#                     description: Faker::Lorem.paragraph_by_chars,
+#                     location: Faker::Address.full_address,
+#                     price: rand(1500..5000),
+#                     user_id: field_owner.id)
+#   field_images.each do |image|
+#     field.images.attach(io: image, filename: "#{rand(0..9999)}-field.png", content_type: "image/png")
+#   end
+#   field.featured_img = field.images.first.key
+#   field.save
+#   field_count
+# end
 
-## Creating fields for the test farmers
-puts " - Creating the extra fields for test farmers now..."
+# ## Creating fields for the test farmers
+# puts " - Creating the extra fields for test farmers now..."
 
-test_farmers = ["apple@farmer.com", "orange@farmer.com", "pear@farmer.com"]
-2.times do
-  test_farmers.each do |farmer|
-    unspash_img = "https://source.unsplash.com/640x360?field"
-    img_1 = URI.parse(unspash_img).open
-    img_2 = URI.parse(unspash_img).open
-    img_3 = URI.parse(unspash_img).open
-    img_4 = URI.parse(unspash_img).open
-    img_5 = URI.parse(unspash_img).open
-    field_images = [img_1, img_2, img_3, img_4, img_5]
-    field = Field.new(name: Faker::Movies::StarWars.planet,
-                      size: rand(50..1500),
-                      description: Faker::Lorem.paragraph_by_chars,
-                      location: Faker::Address.full_address,
-                      price: rand(1500..5000),
-                      user_id: User.find_by(landowner: farmer).id)
-    field_images.each do |image|
-      field.images.attach(io: image, filename: "#{rand(0..9999)}-field.png", content_type: "image/png")
-    end
-    field.featured_img = field.images.first.key
-    field.save
-    field_count
-  end
-end
+# test_farmers = ["apple@farmer.com", "orange@farmer.com", "pear@farmer.com"]
+# 2.times do
+#   test_farmers.each do |farmer|
+#     unspash_img = "https://source.unsplash.com/640x360?field"
+#     img_1 = URI.parse(unspash_img).open
+#     img_2 = URI.parse(unspash_img).open
+#     img_3 = URI.parse(unspash_img).open
+#     img_4 = URI.parse(unspash_img).open
+#     img_5 = URI.parse(unspash_img).open
+#     field_images = [img_1, img_2, img_3, img_4, img_5]
+#     field = Field.new(name: Faker::Movies::StarWars.planet,
+#                       size: rand(50..1500),
+#                       description: Faker::Lorem.paragraph_by_chars,
+#                       location: Faker::Address.full_address,
+#                       price: rand(1500..5000),
+#                       user_id: User.find_by(landowner: farmer).id)
+#     field_images.each do |image|
+#       field.images.attach(io: image, filename: "#{rand(0..9999)}-field.png", content_type: "image/png")
+#     end
+#     field.featured_img = field.images.first.key
+#     field.save
+#     field_count
+#   end
+# end
 
-puts " - Added #{Field.count} fields!"
-puts "--- [ Fields Done ] ---"
+# puts " - Added #{Field.count} fields!"
+# puts "--- [ Fields Done ] ---"
 puts "--- [ Bookings ] ---"
 puts " - Removing all bookings!"
 Booking.destroy_all
@@ -100,14 +100,13 @@ date_from_setter = Date.today;
 date_to_setter = date_from_setter + rand(7..30)
 
 5.times do
-  field_setter = Field.all.sample
+  
   booking = Booking.new(date_from: date_from_setter,
                         date_to: date_to_setter,
-                        price_per_day: field_setter.price,
-                        status: ["pending", "confirmed", "rejected"].sample,
+                        status: rand(0..2),
                         user: User.where("landowner = false").sample,
                         field: Field.all.sample)
-  booking.save
+  booking.save!
   date_from_setter += 31
   date_to_setter = date_from_setter + rand(7..30)
   puts " - Created booking #{booking.date_from} to #{booking.date_to}"
