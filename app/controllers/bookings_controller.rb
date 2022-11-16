95f9e5c3079d1daf0178d015b022158d7391ba86
Hafid Qarchi <haqarchi@gmail.com>
2022-11-16 12:46:38 +0900

    CAX a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,6 +17,13 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    authorize @booking
+    redirect_to bookings_path, status: :see_other
+  end
+
   private
 
   def bookings_params
