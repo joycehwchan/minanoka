33f372a753e02f57a5860a746572933e52555edd
Hafid Qarchi <haqarchi@gmail.com>
2022-11-18 13:57:41 +0900

    refactord the review show page

---
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="insert-in-list"
+export default class extends Controller {
+  connect() {
+    console.log("hi from insert-in-list");
+  }
+}
