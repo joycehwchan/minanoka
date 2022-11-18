cd5b0f1f1fcf3fe6ffdefe273acc041e0aa41b57
twk-mn <fred@tawakemono.com>
2022-11-18 12:05:17 +0900

    Fixed the restriction for who can post a review on the fields

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,10 @@ class Review < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
   validates :rating, presence: true
+  validate :validate_user_for_review
+
+  def validate_user_for_review
+    errors.add(:base, "You haven't booked this field previusly!") unless Booking.where("user_id = ? AND field_id = ?", user.id, field.id).length >= 1
+    # Add date_to in the condition for proper restrictions.
+  end
 end
